d422e30cf39f401905fa3d50a7797290967daa26
kris.kensy <kriskensy@proton.me>
2025-10-27 20:13:09 +0100

    Add handleCityChange function to WetaherBox. Add handleSubmit to PickCity

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,11 +1,17 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
+import { useCallback } from 'react';
 
 const WeatherBox = props => {
+
+  const handleCityChange = useCallback((city) => {
+    console.log('city: ', city);
+  }, []);
+
   return (
     <section>
-      <PickCity />
+      <PickCity action={handleCityChange}/>
       <WeatherSummary />
       <Loader />
     </section>
